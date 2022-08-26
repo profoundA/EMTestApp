0dbd20f55d731c799ee23ddc50143867af0e966a
profoundA <81711479+profoundA@users.noreply.github.com>
2022-08-26 14:58:29 +0300

    Initial Commit

---
+++ EMTestApp/EMTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EMTestAppApp.swift
+//  EMTestApp
+//
+//  Created by Andrey Lobanov on 26.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct EMTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
